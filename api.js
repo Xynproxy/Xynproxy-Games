3c566af582687c2238da3bd2f89e12bcee76d172
Rhonod <148888480+Rhonod@users.noreply.github.com>
2024-10-17 21:07:52 -0500

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-<script>
-  fetch('https://sites.google.com/view/xynproxy/home')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('output').textContent = data.message;
-    });
-</script>
-
-<div id="output">Loading...</div>
